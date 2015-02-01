d73fa3ba265a84b2ceedd879c43e8483fd885a7a
Ebrahim Byagowi <ebrahim@gnu.org>
2015-02-01 21:36:03 +0000

    Remove dir=auto from firstHeading
    Follow up to Ibf408bc6c5d2f8c05667107a547b0b02e06de270
    
    Bug: T51093
    Change-Id: I96aacfa725f05855c6e1541dbda68ecb5bdad757

--- MonoBookTemplate.php
+++ MonoBookTemplate.php
@@ -60,7 +60,7 @@ class MonoBookTemplate extends BaseTemplate {
 				$this->data['pageLanguage'] =
 					$this->getSkin()->getTitle()->getPageViewLanguage()->getHtmlCode();
 				$this->text( 'pageLanguage' );
-				?>"><span dir="auto"><?php $this->html( 'title' ) ?></span></h1>
+				?>"><?php $this->html( 'title' ) ?></h1>
 
 				<div id="bodyContent" class="mw-body-content">
 					<div id="siteSub"><?php $this->msg( 'tagline' ) ?></div>
